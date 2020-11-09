8ced0a2e69e6959b7fd68acfc529cd133f9445ff
Luk1Pr0 <lpytel16@gmail.com>
2020-11-09 21:26:00 +0100

    Added some changes to the functionality

--- script.js
+++ script.js
@@ -11,7 +11,14 @@ function showModal() {
     websiteNameEl.focus();
 }
 
+// Handle data from the form
+function storeBookmark(e) {
+    e.preventDefault();
+    console.log(e);
+}
+
 // Event listeners
 modalShow.addEventListener("click", showModal);
 modalClose.addEventListener("click", () => modal.classList.remove("show-modal"));
 window.addEventListener("click", (e) => (e.target === modal ? modal.classList.remove("show-modal") : false));
+bookmarkForm.addEventListener("submit", storeBookmark);
